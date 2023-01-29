badc304cf094bf93b1169651090706de545aa9ad
ramsal <ramonsalado@gmail.com>
2023-01-29 21:58:30 +0000

    another commit

--- commit.sh
+++
@@ -1,6 +0,0 @@
-cd /home/ubuntu/BlackListIP/
-/usr/bin/git push origin main
-/usr/bin/git add .
-/usr/bin/git commit -m "another commit"
-#/usr/bin/git remote add origin git@github.com:ramsal/BlackListIP.git
-/usr/bin/git push origin master
